2dfe6938e6cb8b77f4e1e000833f20b43fb34089
gleyson003 <lop23031@byui.edu>
2025-01-25 21:57:04 -0300

    Week 3 individual activity.

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,14 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
 import loadHeaderFooter from "./utils.mjs"; 
+import productsListing from "./product-listing.js"; 
 
 const dataSource = new ProductData("tents");
 const element = document.querySelector(".product-list");
 const listing = new ProductList("Tents", dataSource, element);
+const headerAndFooter = loadHeaderFooter.loadHeaderFooter();
+const listingProducts = productsListing.loadHeaderFooter();
+
 
 listing.init();
+headerAndFooter.init();
